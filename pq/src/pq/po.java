2e83d462fd5350d9518af44f27562aa198451a2f
Shree <Shree@Shree-PC>
2017-09-28 11:40:45 +0530

    pq commit

---
+++ pq/src/pq/po.java
@@ -0,0 +1,17 @@
+package pq;
+
+import java.util.Scanner;
+
+public class po {
+	public static void main(String args[])
+	{
+		int a,b,c;
+		System.out.println("Enter two numbers:=");
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		b=in.nextInt();
+		c=a-b;
+		System.out.println("subtract of two numbers:="+c);
+	}
+
+}
